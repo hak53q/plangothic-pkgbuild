19d8135c3ac14788603b5dc59c9f80695cc34f2e
hak53q <hak53q@gmail.com>
2025-04-20 20:33:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ if [ -d "$dir/.git" ]; then
     read -p " ${cyan}$dir${reset} 已存在，是否覆蓋該目錄? [Y/n] " answer
     if [[ -z "$answer" || "${answer,,}" =~ ^(y|yes)$ ]]; then
         git -C "$dir" reset --hard
-        cd "$dir" && makepkg -si && cd || exit 1
+        cd "$dir" && makepkg -sif && cd || exit 1
     else
         exit 1
     fi
 else
     git clone "$url" "$dir"
-    cd "$dir" && makepkg -si && cd || exit 1
+    cd "$dir" && makepkg -sif && cd || exit 1
 fi
