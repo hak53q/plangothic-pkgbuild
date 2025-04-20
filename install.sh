26226f39f5fb1f6399e27983def0ebbbf5daf049
hak53q <hak53q@gmail.com>
2025-04-20 19:51:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
-url=https://github.com/hak53q/Plangothic
-dir=~/Plangothic
+#!/bin/bash
+
+url="https://github.com/hak53q/Plangothic"
+dir="$HOME/Plangothic"
 cyan=$(tput setaf 6)
 reset=$(tput sgr0)
 
-if [ -d "Plangothic/.git" ]; then
+if [ -d "$dir/.git" ]; then
     read -p " ${cyan}$dir${reset} 已存在，是否覆蓋該目錄? [Y/n] " answer
-	if [[ -z "$answer" || "${answer,,}" =~ ^(y|yes)$ ]]; then
-	    git -C $dir reset --hard
-	    cd ~/Plangothic/ && makepkg -si && cd
-	else
-	    exit 1
-	fi
+    if [[ -z "$answer" || "${answer,,}" =~ ^(y|yes)$ ]]; then
+        git -C "$dir" reset --hard
+        cd "$dir" && makepkg -si && cd || exit 1
+    else
+        exit 1
+    fi
 else
-    git clone $url
-    cd ~/Plangothic/ && makepkg -si && cd
+    git clone "$url" "$dir"
+    cd "$dir" && makepkg -si && cd || exit 1
 fi
